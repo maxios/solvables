edfd05648c552e9277abb5b539881ac27165841b
Khaled Maher <maxios.code@gmail.com>
2020-08-16 12:22:38 +0200

    add init solution for counting valleys problem

---
+++ counting_valleys/ruby.rb
@@ -0,0 +1,32 @@
+#!/bin/ruby
+
+require 'json'
+require 'stringio'
+
+# Complete the countingValleys function below.
+def countingValleys(n, s)
+    altitude = 0
+    vallies = 0
+    for step in s.split('')
+        current = altitude
+        altitude += 1 if step == 'U'
+        altitude -= 1 if step == 'D'
+        vallies += 1 if current == -1 && altitude == 0
+    end
+
+    vallies
+end
+
+fptr = File.open(ENV['OUTPUT_PATH'], 'w')
+
+n = gets.to_i
+
+s = gets.to_s.rstrip
+
+result = countingValleys n, s
+
+fptr.write result
+fptr.write "\n"
+
+fptr.close()
+
